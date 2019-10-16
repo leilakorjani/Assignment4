798e78843df017e6b9daf8df479ef95be7ab5f4b
Leila Korjani <Leila.Korjani@student.csulb.edu>
2019-10-16 16:26:38 -0700

    5th commit

--- main.js
+++ main.js
@@ -1,36 +1,36 @@
 document.addEventListener('keyup', keyIHandler);
+
 function keyIHandler (event){
-    let myImage = document.getElementById("myImg")
-    if((myImage.width > 25 && myImage.width < 60) && (event.code == 'KeyI')){
-        myImage.height += 10;
-        myImage.width += 10;
-      
-    }else if((myImage.width == 60) && (event.code == 'KeyI')){
-        myImage.width += 10;
-        myImage.height += 10;
-        myImage.src = "image/explosion.png";
-    }else if ((myImage.width > 60) && (event.code == 'KeyI')){
+    let myEmoji = document.getElementById("balloonEmoji"); 
+    let emojiSize = window.getComputedStyle(document.getElementById("balloonEmoji")).fontSize;
+    let emojiSizeNum = parseInt(emojiSize, 10);
+
+    if((emojiSizeNum > 0 && emojiSizeNum < 50) && (event.code == 'KeyI')){
+        emojiSizeNum += 10;
+        myEmoji.style.fontSize = `${emojiSizeNum}px`
+
+    }else if((emojiSizeNum == 50 ) && (event.code == 'KeyI')){
+        myEmoji.innerText="💥";
+
+    }else if ((emojiSizeNum > 60) && (event.code == 'KeyI')){
         document.removeEventListener("keyup", keyIHandler, true);
     }
 }
 
 document.addEventListener('keyup', keyDHandler);
-function keyDHandler(event){
-    let myImage = document.getElementById("myImg")
-    if((myImage.width > 10 && myImage.width <60) && (event.code == 'KeyD')){
-        myImage.width -= 10;
-        // myImage.height -= 10;
-    }else if((myImage.width == 10) && (event.code == 'KeyD')){
-        myImage.width -= 10;
-        myImage.height -= 10;
-        let myDiv = document.getElementById('myDiv');
-        let newElement = document.createElement('p');
-        myDiv.appendChild(newElement);
-        myDiv.appendChild(newElement);
-        newElement.innerText="Done"
-    }else if((myImage.width < 0) && (event.code == 'KeyD')){
-        document.removeEventListener("keyup", keyDHandler, true);
+function keyDHandler (event){
+    let myEmoji = document.getElementById("balloonEmoji"); 
+    let emojiSize = window.getComputedStyle(document.getElementById("balloonEmoji")).fontSize;
+    let emojiSizeNum = parseInt(emojiSize, 10);
+
+    if((emojiSizeNum > 10 && emojiSizeNum <= 60) && (event.code == 'KeyD')){
+        emojiSizeNum -= 10;
+        myEmoji.style.fontSize = `${emojiSizeNum}px`
+
+    }else if((emojiSizeNum == 10) && (event.code == 'KeyD')){
+        myEmoji.innerHTML = "Done";
+
+    }else if ((emojiSizeNum < 0) && (event.code == 'KeyD')){
+        document.removeEventListener("keyup", keyIHandler, true);
     }
 }
-
-
